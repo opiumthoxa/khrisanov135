083e6efe249acb00c6a792ed6f888bacdeb6367a
Arseniy <xrisik_62@mail.ru>
2022-09-21 17:04:08 +0300

    Add file

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,1 @@
-f_num = int(input('Введите первое число:'))
-oper = input("Введите операцию ")
-s_num = int(input("Введите второе число"))
-if oper == "+":
-    print(f_num + s_num)
-elif oper == "-":
-    print(f_num + s_num)
-elif oper == "*":
-    print(f_num + s_num)
-elif oper == "/":
-    print(f_num + s_num)
-else:
-    print("ошибка")
+print ("hello world")
