fe9f805a42993eb86ff05a8462d7069ced769a87
Arseniy <xrisik_62@mail.ru>
2022-09-21 17:03:16 +0300

    Add file

---
+++ calc.py
@@ -0,0 +1,13 @@
+f_num = int(input('Введите первое число:'))
+oper = input("Введите операцию ")
+s_num = int(input("Введите второе число"))
+if oper == "+":
+    print(f_num + s_num)
+elif oper == "-":
+    print(f_num + s_num)
+elif oper == "*":
+    print(f_num + s_num)
+elif oper == "/":
+    print(f_num + s_num)
+else:
+    print("ошибка")
