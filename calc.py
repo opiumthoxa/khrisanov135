37117e0e536a527274f5404895b57f1baf16beea
Arseniy <xrisik_62@mail.ru>
2022-09-28 19:04:09 +0300

    Add file

--- calc.py
+++ calc.py
@@ -1,15 +1,45 @@
-f_num = int(input('Введите первое число:'))
-oper = input("Введите операцию ")
-s_num = int(input("Введите второе число"))
-prodolzhit = "y"
-if oper == "+":
-    print(f_num + s_num)
-elif oper == "-":
-    print(f_num - s_num)
-elif oper == "*":
-    print(f_num * s_num)
-elif oper == "/":
-    print(f_num / s_num)
-else:
-    print("ошибка")
-prodolzhit = input("Введите 'y', чтобы продолжить, или любую клавишу, чтобы завершить>>")
+class Calculator:
+    def __init__(self):
+        self.stack = []
+        self.token_map = {
+            "+": lambda: self.push(self.pop() + self.pop()),
+            "-": lambda: self.push(self.pop() - self.pop()),
+            "*": lambda: self.push(self.pop() * self.pop()),
+            "/": lambda: self.push(self.pop() / self.pop()),
+            "print": lambda: self.print()
+        }
+
+    def push(self, sth):
+        self.stack.append(int(sth))
+
+    def pop(self):
+        return self.stack.pop()
+
+    def print(self):
+        print(self.pop())
+
+    def run(self, code):
+        tokens = code.split(" ")
+        c = 0
+        while c < len(tokens):
+            current = tokens[c]
+            while c < len(tokens):
+                current = tokens[c]
+                if current.isnumeric():
+                    self.push(current)
+                    c += 1
+                elif current in self.token_map:
+                    self.token_map[current]()
+                    c += 1
+                else:
+                    raise SyntaxError(f"{current}: Invalid syntax.")
+
+
+def repl():
+	calc = Calculator()
+	while True:
+		inpt = input(">> ")
+		calc.run(inpt)
+
+if __name__ == "__main__":
+    repl()
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,40 @@
-print ("hello world")
+class Calculator:
+    def __init__(self):
+        self.stack = []
+        self.token_map = {
+            "+": lambda: self.push(self.pop() + self.pop()),
+            "-": lambda: self.push(self.pop() - self.pop()),
+            "*": lambda: self.push(self.pop() * self.pop()),
+            "/": lambda: self.push(self.pop() / self.pop()),
+            "print": lambda: self.print()
+        }
+
+    def push(self, sth):
+        self.stack.append(int(sth))
+
+    def pop(self):
+        return self.stack.pop()
+
+    def print(self):
+        print(self.pop())
+
+    def run(self, code):
+        tokens = code.split(" ")
+        c = 0
+        while c < len(tokens):
+            current = tokens[c]
+            while c < len(tokens):
+                current = tokens[c]
+                if current.isnumeric():
+                    self.push(current)
+                    c += 1
+                elif current in self.token_map:
+                    self.token_map[current]()
+                    c += 1
+                else:
+                    raise SyntaxError(f"{current}: Invalid syntax.")
+
+
+calc = Calculator()
+calc.run("2 2 + 4 * print")
+
