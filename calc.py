322f2c96f11e0b2255f4858977d240680f9fdb9f
Arseniy <xrisik_62@mail.ru>
2022-09-28 16:19:02 +0300

    Add file

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 f_num = int(input('Введите первое число:'))
 oper = input("Введите операцию ")
 s_num = int(input("Введите второе число"))
+prodolzhit = "y"
 if oper == "+":
     print(f_num + s_num)
 elif oper == "-":
@@ -11,3 +12,4 @@ elif oper == "/":
     print(f_num + s_num)
 else:
     print("ошибка")
+prodolzhit = input("Введите 'y', чтобы продолжить, или любую клавишу, чтобы завершить>>")
